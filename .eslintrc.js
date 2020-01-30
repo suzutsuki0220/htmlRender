a68b45c56ef942335af1ba5649319cd924ebfc09
suzutsuki0220 <suzutsuki0220@gmail.com>
2020-01-31 08:27:55 +0900

    add repository skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest/globals": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "jest"
+    ],
+    "rules": {
+        "no-irregular-whitespace": ["error", {"skipRegExps": true}]
+    }
+};
